7229e29665f1d592790f131803d0b16709edcab8
Karl-maker <karljohanbailey98@gmail.com>
2025-02-23 11:48:18 -0400

    feat: skeleton component

---
+++ src/components/loading/Skeleton.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+const SkeletonLoader = () => {
+  return (
+    <div className="relative w-full h-full bg-gray-300 dark:bg-gray-700 overflow-hidden rounded-md">
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-300 via-gray-200 to-gray-300 dark:from-gray-700 dark:via-gray-600 dark:to-gray-700 animate-shimmer"></div>
+    </div>
+  );
+};
+
+export default SkeletonLoader;
